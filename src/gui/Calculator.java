8fe6e41460a9818f13ef1b703dcacfdc9eba9c18
desmonduz <sarrtv@gmail.com>
2020-11-26 14:29:30 +0500

    added calculator

---
+++ src/gui/Calculator.java
@@ -0,0 +1,38 @@
+package gui;
+
+import javax.swing.*;
+
+public class Calculator extends JFrame {
+    private JPanel rootPane;
+    private JTextField textField1;
+    private JPanel pnlButtons;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JButton button11;
+    private JButton button12;
+
+    public Calculator(){
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+        setTitle("Calculator");
+        add(rootPane);
+    }
+
+}
